46f26699360613e16cb9cb79a2b3c904dd200197
igotapochahontas <igotapochahontas@yahoo.com>
2020-08-20 23:28:40 -0500

    Update export1.js

--- export1.js
+++ export1.js
@@ -32,7 +32,7 @@ function update() {
 scene.background = new THREE.CubeTextureLoader()
 					.setPath( 'assets/' )
 					.
-load( [ 'front.png', 'back.png', 'up.png', 'down.png', 'left.png', 'right.png' ] );
+load( [ 'front.png', 'back.png', 'up.png', 'down.png', 'back.png', 'right.png' ] );
 	var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: scene.background } );
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
